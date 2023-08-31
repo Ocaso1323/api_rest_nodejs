0ad1ed514763f099ed9129faffe89526a7e2326d
agco2002 <agco7331@gmail.com>
2023-08-30 22:23:21 -0500

    creacion de router

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'
-
+import { pool } from './db.js'
+import employeesRoutes from './src/routes/employees.routes.js'
 const app = express()
 
-app.get('/employees',(req,res) => res.send('Obteniendo Empleados'))
-app.post('/employees',(req,res) => res.send('Creando Empleados'))
-app.put('/employees',(req,res) => res.send('Actualizando Empleados'))
-app.delete('/employees',(req,res) => res.send('Eliminando Empleados'))
+
+app.get('/ping' ,async (req,res) => {
+    const [result] = await pool.query('SELECT "pong" AS result')
+    res.json(result [0])
+})
+
+ app.use(employeesRoutes)
+
+
+
 app.listen(3000)
 console.log('servidor en puerto',3000)
